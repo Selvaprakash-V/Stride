5a6fa30227d47f50d8bb0d2ad4b50f5d3453dfa2
Selvaprakash-V <selvaprakashv007@gmail.com>
2026-02-26 22:05:20 +0530

    User API client: add getMe, updateMe, searchUsers, sync from Clerk

--- frontend/src/api/users.js
+++ frontend/src/api/users.js
@@ -1,21 +1,4 @@
 import axiosInstance from "../lib/axios";
-
-export const userApi = {
-  getMe: async () => {
-    const res = await axiosInstance.get("/users/me");
-    return res.data;
-  },
-  updateMe: async (payload) => {
-    const res = await axiosInstance.put("/users/me", payload);
-    return res.data;
-  },
-  searchUsers: async (q) => {
-    const res = await axiosInstance.get(`/users/search?query=${encodeURIComponent(q)}`);
-    return res.data;
-  },
-};
-import axiosInstance from "../lib/axios";
-
 export const userApi = {
   getMe: async () => {
     const response = await axiosInstance.get("/users/me");
